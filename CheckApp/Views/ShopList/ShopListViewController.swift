c015fdfd6f22b8e37615dbb61ab89ef13846c012
Никита Швец <dustikappple@icloud.com>
2022-10-18 13:20:34 +0200

    datafromURL

--- CheckApp/Views/ShopList/ShopListViewController.swift
+++ CheckApp/Views/ShopList/ShopListViewController.swift
@@ -23,6 +23,8 @@ class ShopListViewController: UIViewController, UITableViewDelegate, UITableView
         shopListTableView.register(cellNib, forCellReuseIdentifier: "ShopCell")
         shopListTableView.delegate = self
         shopListTableView.dataSource = self
+        getDataFromURL()
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -78,13 +80,20 @@ extension ShopListViewController {
     func getDataFromURL() {
         let sourceURL = "https://suf.purs.gov.rs/v/?vl=A1E0TU05QUVVUTRNTTlBRVUhPQEADD0BAFxOQQAAAAAAAAABg8eUw9IAAABHpeP5bDf%2FwAM0IsZMIcJcqXwB2bFAawxO718vMBjnaglHEH40hdrrrYKzoDV2M99dCQCth0JRpxpnlYJvtv4bld8wa7HVWANMkz2rSoFx7vdVmrf1Bc6h%2F%2FubDztOO9N29L%2FB06H14TG1guqX3PlOA%2BMuVUxXYoH%2F%2BgKm4b%2Fu12iPrEPeG3TPrxcZMdvtGnNVQbf7UAUTFlnSBtOzSA6NvtrDo5gtxD4jyJaNXvSNF4pAnDWRyiB4DwGVEjBM1senmtcu9S8RqtS0SiiQ43nomD%2BlppSz8Ibd6ZSKouGHB1uPk4xzPc5SsNqfKyHIZ8oC5fO9NxstfOPtL%2Bamx3eLkRwYLr4%2F5jdbuC7PK%2B3VJPQO3J%2FvSn7QqpzbncDX2wEVOcoCTMoUQTbtrBDdw3Q40TeR%2FQ9O6aAh1eZANY91blePoh2y9PjkCOEel4LOvmOtUdwuRkWqkMu6Aw3H6q71tsWRusIcj2QqlT5h30oLCoonVj8ELNuZrvRF4p3vT3047g%2Bzi92rCUavE7CSiTklfXo6cCu676efnQAOhRc%2BkgormMBoFyoU0u5EUnCPORRW6HVnd0PqEy54Cq%2Fgi1Vco0%2BMJdI%2BLycKCALd3TRXwPQiYdvv9g0az7L7Fwiw1V3ffpwFN%2FTPt2DfT6z1Ut%2FFtF3h4o2aJ2TebpahfnpnakiKPOHjFVi7p%2FiE6WoRvow%3D"
         
+        let url = URL(string: sourceURL)!
+        let task = URLSession.shared.dataTask(with: url)
+        {
+            (data, response, error) in
+            guard let data = data else { return }
+            print(String(data: data, encoding: .utf8)!)
+            let tempHTML = String(data: data, encoding: .utf8)!
+        }
+        task.resume()
         
+         
+     
         
     }
-    
-    
-    
-    
 }
 
 
